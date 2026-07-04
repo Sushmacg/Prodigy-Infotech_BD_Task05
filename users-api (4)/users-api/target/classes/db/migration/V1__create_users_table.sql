c2c3d9b5b421affe305319ca4794055f193f536e
Sai Srinivas S <saisrinivasshetty2004@gmail.com>
2026-07-04 21:42:29 +0530

    remove target folder

--- users-api (4)/users-api/target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id CHAR(36) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT uq_users_email UNIQUE (email),
-    CONSTRAINT chk_users_age CHECK (age >= 0 AND age <= 150)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
